bcf911de2973f5555a04ed3591f89b6d034f67ad
mateasmario <mmmateas@gmail.com>
2024-11-14 09:48:31 +0200

    Added some pre-made tests

--- src/test/java/ro/upt/SeleniumTest.java
+++ src/test/java/ro/upt/SeleniumTest.java
@@ -9,9 +9,18 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+/**
+ * It may seem a bit confusing to why I've decided to implement several testing methods using xPath, or arrays, but
+ * it is 100% your choice on how to implement it. Most of the modifications I did to the code were because I wanted
+ * to clean it up a bit. I posted this code just to show you how to use Selenium's syntax, but this doesn't mean
+ * that you should always use these approaches for similar test cases. Try playing with the Inspect Element tool and
+ * implement the rest of the methods on your own. There are still some methods left :)
+ */
 public class SeleniumTest {
     private WebDriver webDriver;
+    private Actions actions;
 
     private static WebDriverManager webDriverManager;
 
@@ -29,6 +38,11 @@ public class SeleniumTest {
         webDriverManager.setup();
         webDriver = webDriverManager.create();
         webDriver.manage().timeouts().implicitlyWait(Duration.of(5, ChronoUnit.SECONDS));
+
+        /*
+            I moved the actions here for clean code purposes, because we'll always use a single reference of it.
+         */
+        actions = new Actions(webDriver);
     }
 
     @AfterEach
@@ -36,26 +50,33 @@ public class SeleniumTest {
         webDriver.quit();
     }
 
+    /*
+        Not a test case, but the login functionality is used in every test case, so I decided to store it in a separate
+        method.
+     */
+    private void login(String username, String password) {
+        WebElement usernameTextBox = webDriver.findElement(By.name("user-name"));
+        actions.sendKeys(usernameTextBox, username);
+
+        WebElement passwordTextBox = webDriver.findElement(By.name("password"));
+        actions.sendKeys(passwordTextBox, password).perform();
+
+        WebElement loginButton = webDriver.findElement(By.name("login-button"));
+        loginButton.click();
+    }
+
     @Test
     public void checkTitleIsCorrect() {
         webDriver.get("https://www.saucedemo.com/");
         String title = webDriver.getTitle();
-        Assertions.assertEquals("SwagLabs", title);
+        Assertions.assertEquals("Swag Labs", title);
     }
 
     @Test
     public void checkAuthenticationWorked() {
         webDriver.get("https://www.saucedemo.com/");
-        Actions actions = new Actions(webDriver);
 
-        WebElement usernameTextBox = webDriver.findElement(By.name("user-name"));
-        actions.sendKeys(usernameTextBox, "standard_user");
-
-        WebElement passwordTextBox = webDriver.findElement(By.name("password"));
-        actions.sendKeys(passwordTextBox, "secret_sauce").perform();
-
-        WebElement loginButton = webDriver.findElement(By.name("login-button"));
-        loginButton.click();
+        login("standard_user", "secret_sauce");
 
         WebElement headingText = webDriver.findElement(By.className("title"));
         Assertions.assertEquals("Products", headingText.getText());
@@ -66,7 +87,12 @@ public class SeleniumTest {
      */
     @Test
     public void testAuthenticationFailed() {
-        // ToDo: Implement
+        webDriver.get("https://www.saucedemo.com/");
+        login("invalid_user", "invalid_password");
+
+        WebElement errorText = webDriver.findElement(By.tagName("h3"));
+        Assertions.assertNotNull(errorText);
+        Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service", errorText.getText());
     }
 
     /*
@@ -75,7 +101,34 @@ public class SeleniumTest {
      */
     @Test
     public void testOrderProductsByPrice() {
-        // ToDo: Implement
+        webDriver.get("https://www.saucedemo.com/");
+        login("standard_user", "secret_sauce");
+
+        /*
+            We first want to check that the initial first item is a backpack
+         */
+        WebElement firstElementTitle = webDriver.findElement(By.className("inventory_item_name"));
+        Assertions.assertNotNull(firstElementTitle);
+        Assertions.assertEquals("Sauce Labs Backpack", firstElementTitle.getText());
+
+        WebElement productSortSelector = webDriver.findElement(By.className("product_sort_container"));
+        productSortSelector.click();
+
+        /*
+            I used xpath here because the option elements do not have a specific identifier (id, name, class, etc.)
+            But you could as well have used findElements(By.tagName("option")), which returns a List, and get the item
+            at index 2. Don't forget, XPath's index starts from 1, and the List index starts from 0!
+         */
+        WebElement productOption = webDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/div/span/select/option[3]"));
+        Assertions.assertNotNull(productOption);
+        productOption.click();
+
+        /*
+            After sorting, we now want to check that the NEW first item is a onesie
+         */
+        firstElementTitle = webDriver.findElement(By.className("inventory_item_name"));
+        Assertions.assertNotNull(firstElementTitle);
+        Assertions.assertEquals("Sauce Labs Onesie", firstElementTitle.getText());
     }
 
     /*
@@ -83,7 +136,36 @@ public class SeleniumTest {
      */
     @Test
     public void testAddToCartDisplaysRemoveButton() {
-        // ToDo: Implement
+        webDriver.get("https://www.saucedemo.com/");
+        login("standard_user", "secret_sauce");
+
+        /*
+            I will get all "Add to cart" buttons and click some of them. It would waste less code rows if I take them
+            as an array and not using their id. But you could as well get the id of each of the buttons you want to
+            press and separately call click() on them. It's your choice how you want to implement it.
+         */
+        List<WebElement> addToCartButtons = webDriver.findElements(By.className("btn_inventory"));
+        Assertions.assertTrue(addToCartButtons.size() >= 6);
+
+        addToCartButtons.get(0).click();
+        addToCartButtons.get(2).click();
+        addToCartButtons.get(5).click();
+
+        /*
+            I am making sure here that the circle indicating the number of products in the cart is equal to the actual
+            number of selected products.
+         */
+        WebElement shoppingCartCounterCircle = webDriver.findElement(By.className("shopping_cart_badge"));
+        Assertions.assertNotNull(shoppingCartCounterCircle);
+        Assertions.assertEquals("3", shoppingCartCounterCircle.getText());
+
+        /*
+            This is the test case where I check if I have three buttons that have changed (when an "Add to cart" button
+            is clicked, it switches to a "Remove" button). You could as well only test one of the two things (counter
+            or buttons changed). It's your choice.
+         */
+        List<WebElement> removeButtons = webDriver.findElements(By.className("btn_secondary")); // The red button has the class btn_secondary, and the normal button is btn_primary (this can be easily observed)
+        Assertions.assertEquals(3, removeButtons.size()); // 3 available remove buttons
     }
 
     /*
@@ -92,6 +174,16 @@ public class SeleniumTest {
      */
     @Test
     public void testEntireCheckoutProcess() {
+        // ToDo: Implement
+    }
+
+    /*
+        After logging in and going further with the checkout process (choose some items and click on the cart),
+        make sure that when you're prompted to type your checkout data, when leaving the "First Name" textbox
+        empty, you'll get an error message.
+     */
+    @Test
+    public void testMissingFirstNameCheckout() {
         // ToDo: Implement
     }
 
